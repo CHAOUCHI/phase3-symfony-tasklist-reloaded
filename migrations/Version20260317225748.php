d1a2a910226f1964b2a869323c9127ff20cae127
CHAOUCHI <massi.chaouchi@gmail.com>
2026-03-21 10:20:32 +0100

    update User.username deafult value

--- migrations/Version20260317225748.php
+++ migrations/Version20260317225748.php
@@ -20,7 +20,7 @@ final class Version20260317225748 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD COLUMN username VARCHAR(255) NOT NULL DEFAULT "username"');
+        $this->addSql('ALTER TABLE user ADD COLUMN username VARCHAR(255) NOT NULL DEFAULT "default"');
     }
 
     public function down(Schema $schema): void
