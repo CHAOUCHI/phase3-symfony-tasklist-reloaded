c3176008753cfc8b6f26348fb3732964041f6f7c
CHAOUCHI <massi.chaouchi@gmail.com>
2026-03-23 18:15:10 +0100

    dashboard static WIP

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(RequestStack $requestStack, LoggerInterface $logger): Response
+    {
+        $session = $requestStack->getSession();
+        $session_array = $session->all();
+        $logger->info("Session data:");
+        foreach($session_array as $key => $value){
+            $logger->info(sprintf("Session key: %s, Session value: %s", $key, $value));
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
