31b3d6b9343c1853e8754893fd8e0ec0eee2f3b2
CHAOUCHI <massi.chaouchi@gmail.com>
2026-03-31 20:59:19 +0200

    print all tasks on the dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,11 +12,11 @@ use Symfony\Component\HttpFoundation\RequestStack;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(RequestStack $requestStack, LoggerInterface $logger): Response
+    public function index(TaskRepository $taskRepo): Response
     {
-
+        $tasks = $taskRepo->findAll();
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'tasks' => $tasks
         ]);
     }
 
