be1cf149d1d7e4f1d339beb28fb1642dec7f0d82
CHAOUCHI <massi.chaouchi@gmail.com>
2026-03-31 13:15:19 +0200

    make priority entity

---
+++ src/Entity/Priority.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PriorityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PriorityRepository::class)]
+class Priority
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $level = null;
+
+    #[ORM\Column]
+    private ?int $importance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLevel(): ?string
+    {
+        return $this->level;
+    }
+
+    public function setLevel(string $level): static
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getImportance(): ?int
+    {
+        return $this->importance;
+    }
+
+    public function setImportance(int $importance): static
+    {
+        $this->importance = $importance;
+
+        return $this;
+    }
+}
