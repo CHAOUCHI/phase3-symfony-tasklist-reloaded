0e84120b5625a225ee42e65decbbd5f72d4a09cd
CHAOUCHi <massi.chaouchi@gmail.com>
2026-04-09 14:37:30 +0200

    display only user's tasks

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,13 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Entity\User;
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(TaskRepository $taskRepo): Response
+    public function index(#[CurrentUser] User $user): Response
     {
-        $tasks = $taskRepo->findAll();
+        $tasks = $user->getTasks();
         return $this->render('dashboard/index.html.twig', [
             'tasks' => $tasks
         ]);
