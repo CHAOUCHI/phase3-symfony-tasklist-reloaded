5bcd7cdce30695847d74cf8730814dbce31c0161
CHAOUCHi <massi.chaouchi@gmail.com>
2026-04-09 18:03:22 +0200

    add priorities values to not yet working filtering form of the dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,15 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use App\Entity\User;
+use App\Repository\PriorityRepository;
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(#[CurrentUser] User $user): Response
+    public function index(#[CurrentUser] User $user, PriorityRepository $priorityRepository): Response
     {
         $tasks = $user->getTasks(); # Get user's tasks only not all tasks in the database
+        $priorities = $priorityRepository->findAll(); # Get all priorities to display them in the dashboard filtering form
         return $this->render('dashboard/index.html.twig', [
-            'tasks' => $tasks
+            'tasks' => $tasks,
+            'priorities' => $priorities,
         ]);
     }
 
