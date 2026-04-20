451feb12858b9687109f316272d7717acf0b27fd
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-04-20 16:06:51 +0200

    conseils Massi css RegistrationFormType

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,7 +17,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',null,[
+                "attr"=>[
+                    "placeholder"=>"Email haha huyhu"
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
