e06817eca45eafdd6729700ff1c7347c71e06836
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-04-22 10:13:28 +0200

    cvs envoyés hier, register front-end finis à 85%

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,31 +17,30 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email',null,[
+             ->add('username',null,[
                 "attr"=>[
-                    "placeholder"=>"Email haha huyhu"
+                    "placeholder"=>"nom_utilisateur"
                 ]
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
-                ],
+            ->add('email',null,[
+                "attr"=>[
+                    "placeholder"=>"votre@email.com"
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                 "placeholder"=>"mot de passe"
+                ],
                 'constraints' => [
                     new NotBlank(
-                        message: 'Please enter a password',
+                        message: 'Entrez un mot de passe',
                     ),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
